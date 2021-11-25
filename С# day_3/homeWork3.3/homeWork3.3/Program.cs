bff790fa336a984ec7164d2cbb7916830bedad99
AzizSultonov535 <79372323+AzizSultonov535@users.noreply.github.com>
2021-11-25 08:47:21 +0300

    C# works
    C# works

---
+++ С# day_3/homeWork3.3/homeWork3.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите a=");
+            double c;
+            double.TryParse(Console.ReadLine(), out c);
+            Console.WriteLine("Введите b=");
+            double d;
+            double.TryParse(Console.ReadLine(), out d);
+            Console.WriteLine(sumAB(c, d));
+            Console.ReadKey(); 
+        }
+
+        static double sumAB(double a, double b)
+        {
+            Console.WriteLine("a+b=");
+            return  a + b;
+        }
+    }
+}
