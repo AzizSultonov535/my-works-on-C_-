5827616ac8dcee34278d737ae0049739564e8d14
AzizSultonov535 <79372323+AzizSultonov535@users.noreply.github.com>
2021-12-01 13:25:29 +0300

    homeWork6
    homeWork6

---
+++ C# day_6/homeWorkDay6.1/homeWorkDay6.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkDay6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Max value: " + int.MaxValue.ToString());
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                proizvedenie(a, b);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+        static void proizvedenie(int a, int b)
+        {
+            if (a*b> int.MaxValue)
+            {
+                throw new OverflowException();
+            }
+            else
+            {
+                Console.WriteLine((a * b).ToString());
+            }
+        }
+    }
+}
