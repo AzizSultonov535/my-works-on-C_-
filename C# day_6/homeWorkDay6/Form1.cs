ac0a8de9596076f448cb61f3f53d5cd70185995e
AzizSultonov535 <79372323+AzizSultonov535@users.noreply.github.com>
2021-12-01 09:57:40 +0300

    homeWork6.2
    homeWork6.2

---
+++ C# day_6/homeWorkDay6/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homeWorkDay6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            try
+            {
+                label1.Text = Convert.ToString(ProizChicel(488557786, 576843));
+            }
+            catch (OverflowException ex)
+            {
+                label1.Text = "a 0 ba barobar shudanash mumkin nest";
+            }
+            catch (Exception ex)
+            {
+                label1.Text = ex.Message;
+            }
+
+                
+            //label1.Text = Convert.ToString(ProizChicel(a, b));
+        }
+        static int ProizChicel(int a, int b)
+        {
+            switch (a)
+            {
+                case 0:
+                    throw new Exception("a 0 ba barobar shudanash mumkin nest");
+                default:
+                    throw new FormatException("раками а слышком дароз шуда бурафт");
+            }
+            return a*b;
+        }
+    }
+}
